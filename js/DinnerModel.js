e8a9b318dd98783ca0ea55c3b0aec17b184f38fd
Gnabriel <elleb@kth.se>
2021-11-03 15:27:08 +0100

    TW1.1

---
+++ js/DinnerModel.js
@@ -0,0 +1,11 @@
+class DinnerModel {
+  constructor(guests = 2) {
+    this.setNumberOfGuests(guests);
+  }
+  setNumberOfGuests(x) {
+    if (x <= 0 || !Number.isInteger(x)) {
+      throw Error("Number of guests has to be a positive non-zero Integer.");
+    }
+    this.numberOfGuests = x;
+  }
+}
