792b353103217fc3d34b959043e45f9cc590d450
Gnabriel <elleb@kth.se>
2021-11-03 16:13:05 +0100

    TW1.2

--- js/views/searchView.js
+++ js/views/searchView.js
@@ -1,12 +1,14 @@
 const SearchFormView = ({ options }) => {
   return (
     <div>
+      <input placeholder="Search..." />
       <select>
         <option>Choose:</option>
         {options.map((opt) => {
           return <option key={options.indexOf(opt)}>{opt}</option>;
         })}
       </select>
+      <button>Search</button>
     </div>
   );
 };
